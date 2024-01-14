f28d1a0c1e310ce438496721ed68a998d355e717
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-01-14 20:49:15 +0530

    Delet Element in Array

---
+++ deletarrayelement.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[7] = {1, 2, 3, 3, 5,3,6};
+    int n;
+    printf("Enter number you want to delete: ");
+    scanf("%d", &n);
+    
+    int newSize = 7; // New size of the array after deletion
+    for (int i = 0; i < newSize; i++) {
+        if (arr[i] == n) {
+            for (int j = i; j < newSize ; j++) {
+                arr[j] = arr[j + 1];
+            }
+            newSize--; // Reduce the size of the array
+            i--; // Check the same index again since it contains a new element now
+        }
+    }
+
+    // Print the modified array
+    for (int i = 0; i < newSize; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
